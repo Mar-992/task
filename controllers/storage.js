cbb06e8f490e42fd87a8a824344418888f5c2017
Marina <marina_stef@hotmail.com>
2021-08-22 20:37:14 +0200

    storage edit

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,3 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 
+
+module.exports ={
+
+    upload: (req, res) => {
+        const file = req.files.image;
+        console.log("file", req.body.id)
+        const maxFileSize = 5 * 1024 * 1024; // 5 MB
+        const allowedTypes = [
+          'image/jpeg',
+          'image/gif',
+          'image/jpg',
+          'image/png'
+        ];
+    
+        if (!allowedTypes.includes(file.mimetype)) {
+          return res.status(400).send({
+            error: true,
+            message: 'Bad request. File type is not allowed.'
+          });
+        }
+    
+        if (file.size > maxFileSize) {
+          return res.status(400).send({
+            error: true,
+            message: 'Bad request. File size exceeds the allowed limit.'
+          });
+        }
+    
+        const uploadsDirectory = path.join(__dirname, '..', 'uploads');
+        if (!fs.existsSync(uploadsDirectory)) {
+          fs.mkdirSync(uploadsDirectory);
+        }
+    
+        const storageDirectory = path.join(__dirname, '..', 'uploads', req.body.id);
+        if (!fs.existsSync(storageDirectory)) {
+          fs.mkdirSync(storageDirectory);
+        }
+    
+        const fileName = `${req.body.id}_${file.name}`;
+    
+        file.mv(`${storageDirectory}/${fileName}`);
+    
+        res.status(201).send({
+          error: false,
+          message: `File with name ${fileName} is uploaded successfully!`
+        });
+      },
+}
--- services/dogs/router.js
+++ services/dogs/router.js
@@ -5,4 +5,4 @@ const controller = require('../../controllers/dog');
 
 router.post('/create', controller.createOne) 
       .get('/list/all', controller.getAll)
-      .get('/:typeOfBreed/image/random', controller.getImage) // ova ne e zavrsheno
+      // .get('/:typeOfBreed/image/random', controller.getImage) // ova ne e zavrsheno
