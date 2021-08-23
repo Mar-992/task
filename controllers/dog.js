983b3a9fa0b6bf91ca164b726a3583535c89556a
Marina <marina_stef@hotmail.com>
2021-08-23 19:20:10 +0200

    error timeout 10s

--- controllers/dog.js
+++ controllers/dog.js
@@ -1,4 +1,6 @@
-const Dog = require("../models/Dog");
+const uuid = require('uuid')
+const Dog = require("../models/dog");
+
 
 
 
@@ -7,8 +9,9 @@ module.exports = {
         try{
             let dog = {
                 ...req.body,
-                dogId: req.dog.id,
+                dogId: uuid.v4,
             };
+            console.log("dog",dog)
             await Dog.create(dog);
             res.status(200).send({
                 error:false,
@@ -17,7 +20,7 @@ module.exports = {
         }catch (err) {
             res.status(500).send({
                 error: true,
-                message: err.message,
+                message: err.message
             });
         }
     },
--- models/dog.js
+++ models/dog.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const dogSchema = mongoose.Schema({
+    id:{
+        type: String,
+        required:["Id is a required field"]
+        
+    },
     breed:{
         type: String,
         required:["Breed is a required field"]
-    },
-    image:{
-        type: String
     }
 })
 
-module.exports = mongoose.model('Dog', dogSchema, 'dogs')
+module.exports = mongoose.model('Dog', dogSchema)
